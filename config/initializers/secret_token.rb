608002a177a585ea031b78ae54d5ee9d09abb985
Deepak kumar padhy <dkrocks2051@gmail.com>
2012-04-13 03:44:30 +0530

    Created a User Resources

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoapp::Application.config.secret_token = '17336ba2d5892004a301e79183db1c655054aaceb774b69de3601230ef9ab5d4a75cc1f93ef1f7bd9ac29cffb052424b966597d5a084ce51f075b8d3f3486973'
